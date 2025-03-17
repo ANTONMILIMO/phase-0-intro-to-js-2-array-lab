e967497f9e2594c56eec8ff2a4d8bb3f86a25f41
Antony Milimo <antonymilimo2017@gmail.com>
2025-03-17 20:42:02 +0300

    added destructive and non-destructive array methods

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Kate) {
+  cats.push(Kate);
+}
+
+function destructivelyPrependCat(Kate) {
+  cats.unshift(Kate);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(Arnold) {
+  return [...cats, Arnold];
+}
+
+function prependCat(Arnold) {
+  return [Arnold, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+
+
